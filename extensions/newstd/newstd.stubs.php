e2d78887936828c5510012a2b44ffe75640dfbad
thepiguy <kunal.a.dandekar@gmail.com>
2024-02-12 17:43:05 +0100

    feat: extended extensions support stage 2

---
+++ extensions/newstd/newstd.stubs.php
@@ -0,0 +1,16 @@
+<?php
+
+// Stubs for newstd
+
+namespace {
+    function hello_world(string $name): string {}
+
+    class Route {
+        public static function init(): int {}
+
+        /**
+         * Start servitude
+         */
+        public static function serve() {}
+    }
+}
