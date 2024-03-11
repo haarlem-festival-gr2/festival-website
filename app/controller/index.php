39e70fc33a3a23f5d123f4a6dafa81e79d341946
Zoran Serdic <35991493+ZoranSerdic@users.noreply.github.com>
2024-03-11 13:29:45 +0100

    cleared index for preparation homepage

--- app/controller/index.php
+++ app/controller/index.php
@@ -1,22 +1,7 @@
 <?php
 
-use Core\Route\Method;
 use Core\Route\Route;
-use Repository\UserRepository;
-
-require_once __DIR__.'/../repository/BaseRepository.php';
-require_once __DIR__.'/../repository/UserRepository.php';
 
 Route::serve('/index', function (array $props) {
-    var_dump($props);
-    var_dump($_SESSION);
-    $name = 'Welcome to our festival website!';
-    Route::render('hello', ['greet' => $name, 'pupils' => ['Ollibolen', 'Poferages', 'I am so sorry for the spellings']]);
-    $repo = new UserRepository();
-    var_dump($repo->get_all());
+    Route::render('index', []);
 });
-
-Route::serve('/index', function (array $props) {
-    $name = 'postman';
-    Route::json(['greet' => $name]);
-}, Method::POST);
