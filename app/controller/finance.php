ead498bbed07e54e5c00541acf3d49ac3f3371f2
thepiguy <kunal.a.dandekar@gmail.com>
2024-04-06 14:07:10 +0200

    info

--- app/controller/finance.php
+++ app/controller/finance.php
@@ -7,13 +7,21 @@ use Service\SoldTicketService;
 
 // maybe eventually /admin/*
 Route::serve('finance', function ($props) {
+    $service = new SoldTicketService();
     $role = @Route::auth()->Role;
     if ($role !== 'admin' && $role !== 'employee') {
         Route::error(ErrorCode::NOT_FOUND);
         exit;
     }
 
-    Route::render('admin.finance', ['monthlyRevenue' => 100, 'totalSales' => 0]);
+    $info = $service->getFiscalInfo();
+    $total = 0;
+
+    foreach ($info as &$row) {
+        $total += $row['Cost'];
+    }
+
+    Route::render('admin.finance', ['monthlyRevenue' => $total, 'totalSales' => count($info)]);
 });
 
 Route::serve('finance', function ($props) {
