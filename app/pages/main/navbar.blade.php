f4900d55e7e170a4497425085aa5225f7dec939d
Mariia <705283@student.inholland.nl>
2024-04-07 04:09:10 +0200

    nav bar responsive but sticky doesnt work

--- app/pages/main/navbar.blade.php
+++ app/pages/main/navbar.blade.php
@@ -1,20 +1,27 @@
-<nav class="h-16 bg-red-100 flex flex-row gap-6 px-4">
-    <a href="/" class="flex gap-2 items-center">
-        <img class="h-12" src="/favicon.ico" alt="">
-        <p class="text-2xl font-bold">Haarlem</p>
-    </a>
+<nav id="stickyNav" class="bg-red-100 px-6 py-3">
+<div class="flex flex-col md:flex-row justify-between items-center w-full">
+        <div class="flex flex-col md:flex-row items-center gap-6">
+            <a href="/" class="flex items-center gap-2">
+                <img class="h-12" src="/favicon.ico" alt="Haarlem Festival">
+                <p class="text-2xl font-bold">Haarlem</p>
+            </a>
 
-    <span class="flex gap-2 items-center text-lg w-full">
-        <a href="/yummy" class="hover:underline">Yummy event</a>
-        <a href="/jazz" class="hover:underline">Jazz event</a>
-        <a href="/history" class="hover:underline">History tours</a>
-        <span class="flex-grow"></span>
-        <a href="/agenda/purchase" class="bg-[#FCC040] px-4 py-2 rounded-lg hover:underline">Attend our festival!</a>
-        @auth
-        <a href="/user" class="hover:underline">My Profile</a>
-        @endauth
-        @authguest
-        <a href="/login" class="hover:underline">Login</a>
-        @endauth
-    </span>
+            <div class="flex flex-col md:flex-row justify-center items-center gap-4">
+                <a href="/yummy" class="hover:underline">Yummy event</a>
+                <a href="/jazz" class="hover:underline">Jazz event</a>
+                <a href="/history" class="hover:underline">History tours</a>
+            </div>
+        </div>
+
+        <div class="flex flex-col md:flex-row items-center gap-6">
+            <a href="/agenda/purchase" class="bg-[#FCC040] px-4 py-2 rounded-lg text-center">Attend our festival!</a>
+            @auth
+                <a href="/user" class="hover:underline">My Profile</a>
+            @endauth
+            @authguest
+                <a href="/login" class="hover:underline">Login</a>
+            @endguest
+        </div>
+    </div>
 </nav>
+
