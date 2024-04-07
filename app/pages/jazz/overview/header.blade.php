ed78801ad0c7b4e0b0cde1fccf4773105622c855
Mariia <705283@student.inholland.nl>
2024-04-07 05:55:36 +0200

    artist page responsive

--- app/pages/jazz/overview/header.blade.php
+++ app/pages/jazz/overview/header.blade.php
@@ -1,7 +1,7 @@
 <div class="relative w-full">
     <img src="{{ $festivalEvent->getImgPath() }}" alt="{{ $festivalEvent->getFestivalEventName() }}" class="w-full object-cover"/>
     <div class="absolute inset-0 flex flex-col justify-center items-center p-4 sm:p-8 text-center text-white">
-        <h1 class="text-3xl sm:text-5xl md:text-7xl font-bold mb-2 font-serif">
+        <h1 class="text-2xl sm:text-4xl md:text-7xl font-medium mb-2 font-serif">
             {{ $festivalEvent->getFestivalEventName() }}
         </h1>
         <p class="text-xl sm:text-2xl md:text-2xl mb-2 font-bold">
